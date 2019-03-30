992e8d3076d46f9a40af34671f19c4bc48ad5701
huang-kun <jack-huang-developer@foxmail.com>
2019-03-30 12:59:30 +0800

    Initial Commit

---
+++ AutoSizingCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoSizingCell
+//
+//  Created by huangkun on 2019/3/30.
+//  Copyright © 2019 huangkun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
